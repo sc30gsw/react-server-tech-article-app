e4b713a58a5557f3f62eb7ae53ee75cf37174a09
Kaito <74711118+sc30gsw@users.noreply.github.com>
2025-03-07 13:52:30 +0900

    ✨ SSGの設定

--- react-server.config.mjs
+++ react-server.config.mjs
@@ -1,5 +1,9 @@
 export default {
   root: 'src/app',
+  export(paths) {
+    // SSGの設定
+    return [...paths, { path: '/popular' }]
+  },
   resolve: {
     alias: {
       '~/': new URL('./src/', import.meta.url).pathname,
